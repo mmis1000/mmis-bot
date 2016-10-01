db898d6554744bc72d86f7862beb14ddcee62725
mmis1000 <mmis1000@yahoo.com.tw>
2016-10-01 09:44:23 +0000

    add test script

--- test/index.js
+++ test/index.js
@@ -7,7 +7,47 @@ coMocha(mocha)
 var assert = chai.assert;
 
 const Core = require("../").Core;
-describe('core', function() {
+describe('Core', function() {
+  describe('addHandle', function () {
+    it('should add handle in same order', function () {
+      var core = new Core();
+      var fisrt = function*(){};
+      var second = function(){};
+      core.addHandle('test', fisrt);
+      core.addHandle('test', second);
+      assert.equal(core.paths.test[1], second);
+    })
+    it('should throw when handler is not function nor generator', function* () {
+      var core = new Core();
+      var err;
+      try {
+      core.addHandle('test', 'bad handle')
+      } catch (e) {
+        err = e;
+      }
+      assert.ok(err.message.match('not a generator function nor function'), 'not throw')
+    })
+  })
+  describe('addHandleBefore', function () {
+    it('should add handle in reverse order', function () {
+      var core = new Core();
+      var fisrt = function*(){};
+      var second = function(){};
+      core.addHandleBefore('test', fisrt);
+      core.addHandleBefore('test', second);
+      assert.equal(core.paths.test[0], second);
+    })
+    it('should throw when handler is not function nor generator', function* () {
+      var core = new Core();
+      var err;
+      try {
+      core.addHandleBefore('test', 'bad handle')
+      } catch (e) {
+        err = e;
+      }
+      assert.ok(err.message.match('not a generator function nor function'), 'not throw')
+    })
+  })
   describe('handle', function () {
     it('should handle the request', function* () {
       var core = new Core();
@@ -131,10 +171,10 @@ describe('core', function() {
     it('should run other path when requested', function* () {
       var core = new Core();
       var count = 0;
-      core.addHandle('test', function (flow) {
+      core.addHandle('test', function* (flow) {
         assert.ok(count === 0)
         count++;
-        flow.runPath('test2');
+        yield flow.runPath('test2');
       })
       core.addHandle('test2', function (flow) {
         assert.ok(count === 1)
@@ -145,7 +185,7 @@ describe('core', function() {
         count++;
       })
       yield core.handle('test');
-      assert.ok(count === 3, 'path not switched properly')
+      assert.ok(count === 3, 'path not ran properly')
     })
   })
   describe('handleParallel', function () {
@@ -225,18 +265,47 @@ describe('core', function() {
       }
       assert.ok(err.message.match('test message'), 'error not throwen')
     })
+    it('should not able terminate request when requested', function* () {
+      var core = new Core();
+      core.addHandle('test', function (flow) {
+        assert.throws(function () {
+          flow.terminate()
+        }, 'terminate is not possible when run parallel');
+      })
+      yield core.handleParallel('test');
+    })
+    it('should not able to resume request when requested', function* () {
+      var core = new Core();
+      core.addHandle('test', function (flow) {
+        assert.throws(function () {
+          flow.resume()
+        }, 'resume is not possible when run parallel');
+      })
+      yield core.handleParallel('test');
+    })
+    it('should not able switch to other path when requested', function* () {
+      var core = new Core();
+      core.addHandle('test', function (flow) {
+        assert.throws(function () {
+          flow.switchPath()
+        }, 'switchPath is not possible when run parallel');
+      })
+      yield core.handleParallel('test');
+    })
+    it('should run other path when requested', function* () {
+      var core = new Core();
+      var count = 0;
+      core.addHandle('test', function* (flow) {
+        assert.ok(count === 0)
+        count++;
+        yield flow.runPath('test2');
+      })
+      core.addHandle('test2', function (flow) {
+        assert.ok(count === 1)
+        count++;
+      })
+      yield core.handle('test');
+      assert.ok(count === 2, 'path not ran properly')
+    })
   })
 })
-
-/*
-it('should throw', function * () {
-  yield new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      reject(new Error('intend error'));
-    }, 1000)
-  })
-})
-it('should also throw', function * () {
-  throw new Error('intend error')
-})
-*/
